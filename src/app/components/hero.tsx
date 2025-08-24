c3b4d31b38cd370525a8a5fd9fb37f0b9ca2c00a
NikhilJosephK <nikhilnjk@gmail.com>
2025-08-24 14:07:53 +0530

    Refactor Hero component to enhance layout and animations; consolidate CircularText instances and improve responsiveness with updated spin durations and styling adjustments.

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -35,78 +35,81 @@ export default function Hero() {
   }, []);
 
   return (
-    <section className="mx-auto w-full h-screen relative overflow-hidden">
-      <CircularText
-        text="FRONTEND*DEVELOPER*SINCE*2022*"
-        onHover="speedUp"
-        spinDuration={4}
-        className="h-[500px] w-full font-extralight !absolute !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2"
-      />
-      <CircularText
-        text="FRONTEND*DEVELOPER*SINCE*2022*"
-        onHover="speedUp"
-        spinDuration={6}
-        className="h-[600px] w-full font-extralight !absolute !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2"
-      />
-      <CircularText
-        text="FRONTEND*DEVELOPER*SINCE*2022*"
-        onHover="speedUp"
-        spinDuration={8}
-        className="h-[700px] w-full font-extralight !absolute !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2"
-      />
-      <CircularText
-        text="FRONTEND*DEVELOPER*SINCE*2022*"
-        onHover="speedUp"
-        spinDuration={10}
-        className="h-[800px] w-full font-extralight !absolute !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2"
-      />
-      <div
-        className={`max-w-6xl mx-auto 
-         rounded-3xl absolute top-1/2 tra left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-hidden w-full h-[500px]`}
-      >
-        <div style={{ width: "100%", height: "100%", position: "relative" }}>
-          {GridDistortionComp && (
-            <GridDistortionComp
-              imageSrc="https://images.unsplash.com/photo-1548159417-f283998827c1?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0"
-              grid={10}
-              mouse={0.25}
-              strength={0.15}
-              relaxation={0.9}
-              className="w-full h-full"
-            />
-          )}
-        </div>
+    <section className="mx-auto w-full h-screen relative overflow-hidden px-4">
+      <div className="">
         <CircularText
           text="FRONTEND*DEVELOPER*SINCE*2022*"
           onHover="speedUp"
-          spinDuration={isHelloWorldHidden ? 20 : 2}
-          className="!absolute !w-96 !h-96 !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2 font-extralight"
+          spinDuration={4}
+          className="h-[500px] w-full font-extralight !absolute !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2"
         />
-        {!isHelloWorldHidden && (
-          <MemoSplitText
-            text="Hello World!"
-            className="text-2xl font-medium text-center w-full !absolute !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2"
-            delay={100}
-            duration={0.6}
-            ease="power3.out"
-            splitType="chars"
-            from={from}
-            to={to}
-            threshold={0.1}
-            rootMargin="-100px"
-            textAlign="center"
-            onLetterAnimationComplete={handleAnimationComplete}
-          />
-        )}
-        <h1
-          className={`text-2xl font-medium text-center w-full !absolute !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2 transition-all duration-1000 ${
-            !isHelloWorldHidden
-              ? "opacity-0 scale-200"
-              : "opacity-100 scale-100"
-          }`}
+        <CircularText
+          text="FRONTEND*DEVELOPER*SINCE*2022*"
+          onHover="speedUp"
+          spinDuration={6}
+          className="h-[600px] w-full font-extralight !absolute !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2"
+        />
+        <CircularText
+          text="FRONTEND*DEVELOPER*SINCE*2022*"
+          onHover="speedUp"
+          spinDuration={8}
+          className="h-[700px] w-full font-extralight !absolute !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2"
+        />
+        <CircularText
+          text="FRONTEND*DEVELOPER*SINCE*2022*"
+          onHover="speedUp"
+          spinDuration={10}
+          className="h-[800px] w-full font-extralight !absolute !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2"
+        />
+
+        <div
+          className={`max-w-6xl max-xl:max-w-5xl mx-auto 
+         rounded-3xl max-lg:rounded-none absolute top-1/2 tra left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-hidden w-full h-[500px]`}
         >
-          I&apos;m (Nick)hil
-        </h1>
+          <div style={{ width: "100%", height: "100%", position: "relative" }}>
+            {GridDistortionComp && (
+              <GridDistortionComp
+                imageSrc="https://images.unsplash.com/photo-1548159417-f283998827c1?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0"
+                grid={10}
+                mouse={0.25}
+                strength={0.15}
+                relaxation={0.9}
+                className="w-full h-full"
+              />
+            )}
+          </div>
+          <CircularText
+            text="FRONTEND*DEVELOPER*SINCE*2022*"
+            onHover="speedUp"
+            spinDuration={isHelloWorldHidden ? 20 : 2}
+            className="!absolute !w-96 !h-96 !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2 font-extralight"
+          />
+          {!isHelloWorldHidden && (
+            <MemoSplitText
+              text="Hello World!"
+              className="text-2xl font-medium text-center w-full !absolute !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2"
+              delay={100}
+              duration={0.6}
+              ease="power3.out"
+              splitType="chars"
+              from={from}
+              to={to}
+              threshold={0.1}
+              rootMargin="-100px"
+              textAlign="center"
+              onLetterAnimationComplete={handleAnimationComplete}
+            />
+          )}
+          <h1
+            className={`text-2xl font-medium text-center w-full !absolute !top-1/2 !left-1/2 !-translate-x-1/2 !-translate-y-1/2 transition-all duration-1000 ${
+              !isHelloWorldHidden
+                ? "opacity-0 scale-200"
+                : "opacity-100 scale-100"
+            }`}
+          >
+            I&apos;m (Nick)hil
+          </h1>
+        </div>
       </div>
     </section>
   );
